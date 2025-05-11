bfe60cf069d9fd7bb16428e68eca96fab1be5934
Meagan Oakley <meagan@Meagans-MacBook-Pro-5.local>
2025-05-11 14:52:50 +0100

    select button support

--- buttons.py
+++ buttons.py
@@ -8,11 +8,12 @@ import signal
 import colorsys
 import math
 
+
 hit_button = Button(4, bounce_time=0.05)
-#select_button = Button(3, bounce_time=0.05)
-#press_count = 0
-#press_print = ""
-press_print = "Cerium"
+select_button = Button(3, bounce_time=0.05)
+press_count = 0
+press_print = ""
+#press_print = "Cerium"
 
 
 led_on = False
@@ -22,30 +23,31 @@ ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=True, pixel_order=ORDER)
 
 
-#def presses():
-#    global press_count, press_print
-#    press_count = (press_count % 3) + 1
-#
-#    if press_count == 1:
-#        press_print = "Cesium"
-#    if press_count == 2:
-#        press_print="Potassium"
-#    if press_count == 3:
-#        press_print="Cerium"
+def presses():
+    global press_count, press_print
+    press_count = (press_count % 3) + 1
 
-#    print(press_print)
+    pixels.fill((0,0,0))
+
+    if press_count == 1:
+        press_print = "Cesium"
+    if press_count == 2:
+        press_print="Potassium"
+    if press_count == 3:
+        press_print="Cerium"
+
+    print(press_print)
 
 def shutdown_handler(sig, frame):
     print("\nExiting... Turning off LEDs.")
     pixels.fill((0, 0, 0))
     hit_button.close()
+    select_button.close()
     sys.exit(0)
 
-# Register signal handlers
 signal.signal(signal.SIGINT, shutdown_handler)
 signal.signal(signal.SIGTERM, shutdown_handler)
 
-# --- Main Action ---
 def hit_action():
     global press_print, led_on
 
@@ -66,7 +68,7 @@ def hit_action():
 
     active_pixels = int((num_pixels // 2) * percent)
     for i in range(active_pixels):
-        hue = i / active_pixels  
+        hue = i / active_pixels
         r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
         color = (int(r * 255), int(g * 255), int(b * 255))
 
@@ -80,6 +82,7 @@ def hit_action():
     led_on = True
 
 hit_button.when_pressed = hit_action
+select_button.when_pressed = presses
 
 print("Ready. Press the button.")
 try:
